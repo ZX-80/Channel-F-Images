fbde5773f7532ab7ac84631aed4a34ba8a74075f
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-08-25 12:36:01 -0400

    Refactor
    Mainly focused on get_memory_map(), but also error messages and formatting

--- converter/bin2chf.py
+++ converter/bin2chf.py
@@ -11,7 +11,7 @@ import functools
 import sys
 
 from io import BufferedWriter
-from typing_extensions import TypeAlias
+from typing import TypeAlias
 
 PROGRAM_NAME = "bin2chf"
 PROGRAM_VERSION = "1.0"
@@ -20,13 +20,11 @@ uint8: TypeAlias = int
 uint16: TypeAlias = int
 uint32: TypeAlias = int
 
-
-
 class ChipType:
     def __init__(self, designation_id: uint16, string: str) -> None:
         self.designation_id = designation_id
-        self.string = string
-        setattr(ChipType, string, designation_id)
+        self.string = string.lower()
+        setattr(ChipType, string.upper(), designation_id)
 
 class Packet:
     def __init__(self, chip_type: uint16, load_address: uint16, image_size: uint16, data: list[uint8] = None, bank_number: uint16 = 0) -> None:
@@ -58,19 +56,19 @@ chip_type_list = [
 ]
 
 hardware_type_list = [
-    HardwareType(
+    HardwareType( # Videocart
         0,
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0xF800)
         ]
     ),
-    HardwareType(
+    HardwareType( # Videocarts 10 / 18 (with 2102 SRAM)
         1,
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0xF800)
         ]
     ),
-    HardwareType(
+    HardwareType( # ROM + RAM (with 3853 SMI)
         2,
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
@@ -78,7 +76,7 @@ hardware_type_list = [
             Packet(chip_type=ChipType.ROM, load_address=0x3000, image_size=0xC800),
         ]
     ),
-    HardwareType(
+    HardwareType( # SABA Videoplay 20
         3,
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
@@ -88,8 +86,10 @@ hardware_type_list = [
             Packet(chip_type=ChipType.ROM, load_address=0x4000, image_size=0xB800),
         ]
     ),
-    HardwareType(4, []),
-    HardwareType(
+    HardwareType( # Multi-Cart
+        4, []
+    ),
+    HardwareType( # Flashcart
         5,
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
@@ -155,23 +155,23 @@ def parse_args() -> argparse.Namespace:
 
     for chip_type in chip_type_list:
         parser.add_argument(
-            f"-{chip_type.string.lower()}",
-            f"--{chip_type.string.lower()}",
+            f"-{chip_type.string}",
+            f"--{chip_type.string}",
             nargs=2,
             metavar=("START", "SIZE"),
             type=functools.partial(int, base=0),
             default=[],
             action="append",
-            help=f"designates a range of memory as {chip_type.string}",
+            help=f"designates a range of memory as {chip_type.string.upper()}",
         )
 
     return parser.parse_args()
 
 def validate_and_fetch_infile() -> bytes:
     if args.infile.suffix != ".bin":
-        sys.exit(f"[ERROR] \"{args.infile}\" is not a .bin file")
+        sys.exit(f"[ERROR] File \"{args.infile}\" is not a .bin file")
     if not args.infile.is_file():
-        sys.exit(f"[ERROR] \"{args.infile}\" does not exist")
+        sys.exit(f"[ERROR] File \"{args.infile}\" does not exist")
     try:
         with open(args.infile, mode='rb') as infile_fp:
             return infile_fp.read()
@@ -186,7 +186,7 @@ def validate_and_fetch_outfile() -> BufferedWriter:
         if not args.yes and not args.no: # Prompt user
             response = input(f"File {args.outfile} already exists. Overwrite? [y/N] ")
         if args.no or response != 'y':
-            sys.exit(f"[ERROR] \"{args.outfile}\" already exists")
+            sys.exit(f"[ERROR] File \"{args.outfile}\" already exists")
         else:
             print(f"[WARNING] Overwriting \"{args.outfile}\"")    
     try:
@@ -194,40 +194,40 @@ def validate_and_fetch_outfile() -> BufferedWriter:
     except OSError:
         sys.exit(f"[ERROR] File \"{args.outfile}\" could not be opened/written")
 
+def generate_arg_text(packet: Packet) -> str:
+    command = f"--{chip_type_list[packet.chip_type].string}"
+    start = hex(packet.load_address)
+    size = hex(packet.image_size)
+    return f"{command} {start} {size}"
+
 def get_memory_map() -> list[Packet]:
     packets = hardware_type_list[args.hardwaretype].packets
-    if sum(len(getattr(args, chip_type.string.lower())) for chip_type in chip_type_list) != 0: # Memory map provided by user?
+    
+    # Memory map provided by user?
+    if True in (bool(getattr(args, chip_type.string)) for chip_type in chip_type_list):
         if hardware_type_list[args.hardwaretype].manual_memory_map:
+            # Build packets
             packets = []
-            chip_type = 0
-            for provided_ranges in (getattr(args, chip_type.string.lower()) for chip_type in chip_type_list):
-                for start, size in provided_ranges:
-                    packets.append(Packet(chip_type=chip_type, load_address=start, image_size=size))
-                chip_type += 1
+            for chip_type in chip_type_list:
+                for start, size in getattr(args, chip_type.string):
+                    packets.append(Packet(chip_type.designation_id, start, size))
 
-            if len(packets) > 0:
-                # Validate packet ranges
-                for packet in packets:
-                    
-                    arg_text = "--" + chip_type_list[packet.chip_type].string.lower() + " " + hex(packet.load_address) + " " + hex(packet.image_size)
-                    if not 0x800 <= packet.load_address <= 0xffff:
-                        sys.exit(f"[ERROR] Load address \"{hex(packet.load_address)}\" in \"{arg_text}\" is invalid. Must be between 0x0800 & 0xffff")
-                    if not 1 <= packet.image_size <= 0xf800:
-                        sys.exit(f"[ERROR] Size \"{hex(packet.image_size)}\" in \"{arg_text}\" is invalid. Must be between 0x0001 & {hex(0x10000 - packet.load_address)}")
-                    if packet.load_address + packet.image_size > 0x10000:
-                        sys.exit(f"[ERROR] \"{arg_text}\" ranges from {hex(packet.load_address)} to {hex(packet.load_address + packet.image_size - 1)} which exceeds 0xFFFF")
+            # Validate packet ranges
+            for packet in packets:
+                if not 0x800 <= packet.load_address <= 0xffff:
+                    sys.exit(f"[ERROR] Load address \"0x{packet.load_address:x}\" in \"{generate_arg_text(packet)}\" is invalid. Must be between 0x0800 & 0xffff")
+                if not 1 <= packet.image_size <= 0xf800:
+                    sys.exit(f"[ERROR] Size \"0x{packet.image_size:x}\" in \"{generate_arg_text(packet)}\" is invalid. Must be between 0x0001 & 0x{0x10000 - packet.load_address:x}")
+                if packet.load_address + packet.image_size > 0x10000:
+                    sys.exit(f"[ERROR] Packet \"{generate_arg_text(packet)}\" is out of bounds. Range 0x{packet.load_address:x} to 0x{packet.load_address + packet.image_size - 1:x} exceeds 0xffff")
 
-                # Check for overlapping packets      
-                packets = sorted(packets, key=lambda packet: packet.load_address)
-                prev_packet = packets[0]
-                for packet in packets[1:]:
-                    if packet.load_address < prev_packet.load_address + prev_packet.image_size:
-                        prev_arg_text = "--" + chip_type_list[prev_packet.chip_type].string.lower() + " " + hex(prev_packet.load_address) + " " + hex(prev_packet.image_size)
-                        arg_text = "--" + chip_type_list[packet.chip_type].string.lower() + " " + hex(packet.load_address) + " " + hex(packet.image_size)
-                        sys.exit(f"[ERROR] packet {arg_text} overlaps packet {prev_arg_text}")
-                    prev_packet = packet
+            # Check for overlapping packets
+            packets = sorted(packets, key=lambda packet: packet.load_address)
+            for prev_packet, packet in zip(packets[::2], packets[1::2]):
+                if packet.load_address < prev_packet.load_address + prev_packet.image_size:
+                    sys.exit(f"[ERROR] Packet \"{generate_arg_text(packet)}\" overlaps packet \"{generate_arg_text(prev_packet)}\"")
         else:
-            print("[WARNING] hardware type doesn't support manual memory map")
+            print("[WARNING] Hardware type doesn't support manual memory map")
     return packets
 
 if __name__ == "__main__":
