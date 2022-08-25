ca41bf4353b32b94ffed3ef5961cd6fc67fe356a
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-08-25 09:49:06 -0400

    Refactored chip type
    Chip type was refactored to ease the addition of more chip types.

--- converter/bin2chf.py
+++ converter/bin2chf.py
@@ -20,17 +20,27 @@ uint8: TypeAlias = int
 uint16: TypeAlias = int
 uint32: TypeAlias = int
 
+
+
+class ChipType:
+    def __init__(self, designation_id: uint16, string: str) -> None:
+        self.designation_id = designation_id
+        self.string = string
+        setattr(ChipType, string, designation_id)
+
 class Packet:
-    ROM = 0
-    RAM = 1
-    LED = 2
-    NVRAM = 3
     def __init__(self, chip_type: uint16, load_address: uint16, image_size: uint16, data: list[uint8] = None, bank_number: uint16 = 0) -> None:
         self.chip_type = chip_type
         self.bank_number = bank_number
         self.load_address = load_address
         self.image_size = image_size
         self.data = data
+
+class HardwareType:
+    def __init__(self, designation_id: uint16, packets: list[Packet], manual_memory_map: bool = False) -> None:
+        self.designation_id = designation_id # 2
+        self.packets = packets # packets layout
+        self.manual_memory_map = manual_memory_map # supports manual memory maps
 
 class ChfData:
     def __init__(self, hardware_type: uint16, version: float, title: str, extra: str, packets: list[Packet]) -> None:
@@ -40,50 +50,51 @@ class ChfData:
         self.extra = extra
         self.packets = packets
 
-class HardwareType:
-    def __init__(self, designation_id: uint16, packets: list[Packet], manual_memory_map: bool = False) -> None:
-        self.designation_id = designation_id # 2
-        self.packets = packets # packets layout
-        self.manual_memory_map = manual_memory_map # supports manual memory maps
+chip_type_list = [
+    ChipType(0, "ROM"),
+    ChipType(1, "RAM"),
+    ChipType(2, "LED"),
+    ChipType(3, "NVRAM"),
+]
 
 hardware_type_list = [
     HardwareType(
         0,
         [
-            Packet(chip_type=Packet.ROM, load_address=0x800, image_size=0xF800)
+            Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0xF800)
         ]
     ),
     HardwareType(
         1,
         [
-            Packet(chip_type=Packet.ROM, load_address=0x800, image_size=0xF800)
+            Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0xF800)
         ]
     ),
     HardwareType(
         2,
         [
-            Packet(chip_type=Packet.ROM, load_address=0x800, image_size=0x2000),
-            Packet(chip_type=Packet.RAM, load_address=0x2800, image_size=0x800),
-            Packet(chip_type=Packet.ROM, load_address=0x3000, image_size=0xC800),
+            Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
+            Packet(chip_type=ChipType.RAM, load_address=0x2800, image_size=0x800),
+            Packet(chip_type=ChipType.ROM, load_address=0x3000, image_size=0xC800),
         ]
     ),
     HardwareType(
         3,
         [
-            Packet(chip_type=Packet.ROM, load_address=0x800, image_size=0x2000),
-            Packet(chip_type=Packet.RAM, load_address=0x2800, image_size=0x800),
-            Packet(chip_type=Packet.ROM, load_address=0x3000, image_size=0x800),
-            Packet(chip_type=Packet.LED, load_address=0x3800, image_size=0x800),
-            Packet(chip_type=Packet.ROM, load_address=0x4000, image_size=0xB800),
+            Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
+            Packet(chip_type=ChipType.RAM, load_address=0x2800, image_size=0x800),
+            Packet(chip_type=ChipType.ROM, load_address=0x3000, image_size=0x800),
+            Packet(chip_type=ChipType.LED, load_address=0x3800, image_size=0x800),
+            Packet(chip_type=ChipType.ROM, load_address=0x4000, image_size=0xB800),
         ]
     ),
     HardwareType(4, []),
     HardwareType(
         5,
         [
-            Packet(chip_type=Packet.ROM, load_address=0x800, image_size=0x2000),
-            Packet(chip_type=Packet.RAM, load_address=0x2800, image_size=0x800),
-            Packet(chip_type=Packet.ROM, load_address=0x3000, image_size=0xC800),
+            Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
+            Packet(chip_type=ChipType.RAM, load_address=0x2800, image_size=0x800),
+            Packet(chip_type=ChipType.ROM, load_address=0x3000, image_size=0xC800),
         ],
         True
     ),
@@ -124,46 +135,6 @@ def parse_args() -> argparse.Namespace:
         help="the game's title",
     )
     parser.add_argument(
-        "-ram",
-        "--ram",
-        nargs=2,
-        metavar=("START", "SIZE"),
-        type=functools.partial(int, base=0),
-        default=[],
-        action="append",
-        help="designates a range of memory as RAM",
-    )
-    parser.add_argument(
-        "-rom",
-        "--rom",
-        nargs=2,
-        metavar=("START", "SIZE"),
-        type=functools.partial(int, base=0),
-        default=[],
-        action="append",
-        help="designates a range of memory as ROM",
-    )
-    parser.add_argument(
-        "-led",
-        "--led",
-        nargs=2,
-        metavar=("START", "SIZE"),
-        type=functools.partial(int, base=0),
-        default=[],
-        action="append",
-        help="designates a range of memory as LED",
-    )
-    parser.add_argument(
-        "-nvram",
-        "--nvram",
-        nargs=2,
-        metavar=("START", "SIZE"),
-        type=functools.partial(int, base=0),
-        default=[],
-        action="append",
-        help="designates a range of memory as NVRAM",
-    )
-    parser.add_argument(
         "-y",
         "--yes",
         action="store_true",
@@ -181,6 +152,18 @@ def parse_args() -> argparse.Namespace:
         action="version",
         version=f"%(prog)s {PROGRAM_VERSION}"
     )
+
+    for chip_type in chip_type_list:
+        parser.add_argument(
+            f"-{chip_type.string.lower()}",
+            f"--{chip_type.string.lower()}",
+            nargs=2,
+            metavar=("START", "SIZE"),
+            type=functools.partial(int, base=0),
+            default=[],
+            action="append",
+            help=f"designates a range of memory as {chip_type.string}",
+        )
 
     return parser.parse_args()
 
@@ -211,13 +194,13 @@ def validate_and_fetch_outfile() -> BufferedWriter:
     except OSError:
         sys.exit(f"[ERROR] File \"{args.outfile}\" could not be opened/written")
 
-def get_manual_memory_map() -> list[Packet]:
-    packets = hardware_type_list[args.hardwaretype].packets 
-    if len(args.rom + args.ram + args.led + args.nvram) != 0: # Memory map provided by user
+def get_memory_map() -> list[Packet]:
+    packets = hardware_type_list[args.hardwaretype].packets
+    if sum(len(getattr(args, chip_type.string.lower())) for chip_type in chip_type_list) != 0: # Memory map provided by user?
         if hardware_type_list[args.hardwaretype].manual_memory_map:
             packets = []
             chip_type = 0
-            for provided_ranges in [args.rom, args.ram, args.led, args.nvram]:
+            for provided_ranges in (getattr(args, chip_type.string.lower()) for chip_type in chip_type_list):
                 for start, size in provided_ranges:
                     packets.append(Packet(chip_type=chip_type, load_address=start, image_size=size))
                 chip_type += 1
@@ -225,11 +208,12 @@ def get_manual_memory_map() -> list[Packet]:
             if len(packets) > 0:
                 # Validate packet ranges
                 for packet in packets:
-                    arg_text = "--" + ["rom", "ram", "led", "nvram"][packet.chip_type] + " " + hex(packet.load_address) + " " + hex(packet.image_size)
+                    
+                    arg_text = "--" + chip_type_list[packet.chip_type].string.lower() + " " + hex(packet.load_address) + " " + hex(packet.image_size)
                     if not 0x800 <= packet.load_address <= 0xffff:
                         sys.exit(f"[ERROR] Load address \"{hex(packet.load_address)}\" in \"{arg_text}\" is invalid. Must be between 0x0800 & 0xffff")
                     if not 1 <= packet.image_size <= 0xf800:
-                        sys.exit(f"[ERROR] Size \"{hex(packet.image_size)}\" in \"{arg_text}\" is invalid. Must be between 0x0001 & 0xf800")
+                        sys.exit(f"[ERROR] Size \"{hex(packet.image_size)}\" in \"{arg_text}\" is invalid. Must be between 0x0001 & {hex(0x10000 - packet.load_address)}")
                     if packet.load_address + packet.image_size > 0x10000:
                         sys.exit(f"[ERROR] \"{arg_text}\" ranges from {hex(packet.load_address)} to {hex(packet.load_address + packet.image_size - 1)} which exceeds 0xFFFF")
 
@@ -238,8 +222,8 @@ def get_manual_memory_map() -> list[Packet]:
                 prev_packet = packets[0]
                 for packet in packets[1:]:
                     if packet.load_address < prev_packet.load_address + prev_packet.image_size:
-                        prev_arg_text = "--" + ["rom", "ram", "led", "nvram"][prev_packet.chip_type] + " " + hex(prev_packet.load_address) + " " + hex(prev_packet.image_size)
-                        arg_text = "--" + ["rom", "ram", "led", "nvram"][packet.chip_type] + " " + hex(packet.load_address) + " " + hex(packet.image_size)
+                        prev_arg_text = "--" + chip_type_list[prev_packet.chip_type].string.lower() + " " + hex(prev_packet.load_address) + " " + hex(prev_packet.image_size)
+                        arg_text = "--" + chip_type_list[packet.chip_type].string.lower() + " " + hex(packet.load_address) + " " + hex(packet.image_size)
                         sys.exit(f"[ERROR] packet {arg_text} overlaps packet {prev_arg_text}")
                     prev_packet = packet
         else:
@@ -262,7 +246,7 @@ if __name__ == "__main__":
         args.title = args.infile.stem
 
     # Handle memory map
-    packets = get_manual_memory_map()
+    packets = get_memory_map()
 
     # TODO: map data in .bin file to packets in packets list,
     # deleting/resizing packets as necessary
