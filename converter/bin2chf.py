73fea37316a7256146a15ae29d395e9340bd56d7
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-08-30 22:24:30 -0400

    Refactor
    General refactor (reduced use of global "args", variable names changed, better terminal output, optimizations)

--- converter/bin2chf.py
+++ converter/bin2chf.py
@@ -4,6 +4,7 @@
 """Convert Channel F .bin files into .chf files"""
 
 # TODO: support a config file for command arguments
+# TODO: multi-cart support
 
 import argparse
 import pathlib
@@ -21,16 +22,17 @@ FORMAT_VERSION = "1.0"
 uint8: TypeAlias = int
 uint16: TypeAlias = int
 uint32: TypeAlias = int
+array_of_uint8: TypeAlias = memoryview
 
 class ChipType:
-    def __init__(self, designation_id: uint16, string: str, has_data: bool) -> None:
+    def __init__(self, designation_id: uint16, name: str, has_data: bool) -> None:
         self.designation_id = designation_id
-        self.string = string.lower()
+        self.name = name.lower()
         self.has_data = has_data
-        setattr(ChipType, string.upper(), designation_id)
+        setattr(ChipType, name.upper(), designation_id)
 
 class Packet:
-    def __init__(self, chip_type: uint16, load_address: uint16, image_size: uint16, data: memoryview = None, bank_number: uint16 = 0) -> None:
+    def __init__(self, chip_type: uint16, load_address: uint16, image_size: uint16, data: array_of_uint8 = None, bank_number: uint16 = 0) -> None:
         self.chip_type = chip_type
         self.bank_number = bank_number
         self.load_address = load_address
@@ -38,8 +40,9 @@ class Packet:
         self.data = data
 
 class HardwareType:
-    def __init__(self, designation_id: uint16, packets: list[Packet], manual_memory_map: bool = False) -> None:
+    def __init__(self, designation_id: uint16, name: str, packets: list[Packet], manual_memory_map: bool = False) -> None:
         self.designation_id = designation_id
+        self.name = name
         self.packets = packets
         self.manual_memory_map = manual_memory_map
 
@@ -59,28 +62,24 @@ chip_type_list = [
 ]
 
 hardware_type_list = [
-    HardwareType( # Videocart
-        0,
+    HardwareType(0, "Videocart",
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0xF800)
         ]
     ),
-    HardwareType( # Videocarts 10 / 18 (with 2102 SRAM)
-        1,
+    HardwareType(1, "Videocart 10/18 (with 2102 SRAM)",
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0xF800)
         ]
     ),
-    HardwareType( # ROM + RAM (with 3853 SMI)
-        2,
+    HardwareType(2, "ROM + RAM (with 3853 SMI)",
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
             Packet(chip_type=ChipType.RAM, load_address=0x2800, image_size=0x800),
             Packet(chip_type=ChipType.ROM, load_address=0x3000, image_size=0xC800),
         ]
     ),
-    HardwareType( # SABA Videoplay 20
-        3,
+    HardwareType(3, "SABA Videoplay 20",
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
             Packet(chip_type=ChipType.RAM, load_address=0x2800, image_size=0x800),
@@ -89,17 +88,14 @@ hardware_type_list = [
             Packet(chip_type=ChipType.ROM, load_address=0x4000, image_size=0xB800),
         ]
     ),
-    HardwareType( # Multi-Cart
-        4, []
-    ),
-    HardwareType( # Flashcart
-        5,
+    HardwareType(4, "Multi-Cart", []),
+    HardwareType(5, "Flashcart",
         [
             Packet(chip_type=ChipType.ROM, load_address=0x800, image_size=0x2000),
             Packet(chip_type=ChipType.RAM, load_address=0x2800, image_size=0x800),
             Packet(chip_type=ChipType.ROM, load_address=0x3000, image_size=0xC800),
         ],
-        True
+        manual_memory_map = True
     ),
 ]
 
@@ -124,7 +120,7 @@ def parse_args() -> argparse.Namespace:
         help="the template file name",
     )
     parser.add_argument(
-        "-ht",
+        "-w",
         "--hardwaretype",
         type=int,
         choices=list(range(len(hardware_type_list) + 1)),
@@ -158,61 +154,61 @@ def parse_args() -> argparse.Namespace:
 
     for chip_type in chip_type_list:
         parser.add_argument(
-            f"-{chip_type.string}",
-            f"--{chip_type.string}",
+            f"-{chip_type.name}",
+            f"--{chip_type.name}",
             nargs=2,
             metavar=("START", "SIZE"),
             type=functools.partial(int, base=0),
             default=[],
             action="append",
-            help=f"designates a range of memory as {chip_type.string.upper()}",
+            help=f"designates a range of memory as {chip_type.name.upper()}",
         )
 
     return parser.parse_args()
 
-def validate_and_fetch_infile() -> bytes:
-    if args.infile.suffix != ".bin":
-        sys.exit(f"[ERROR] File \"{args.infile}\" is not a .bin file")
-    if not args.infile.is_file():
-        sys.exit(f"[ERROR] File \"{args.infile}\" does not exist")
+def validate_and_fetch_infile(infile: pathlib.Path) -> bytes:
+    if infile.suffix != ".bin":
+        sys.exit(f"[ERROR] File \"{infile}\" is not a .bin file")
+    if not infile.is_file():
+        sys.exit(f"[ERROR] File \"{infile}\" does not exist")
     try:
-        with open(args.infile, mode='rb') as infile_fp:
+        with open(infile, mode='rb') as infile_fp:
             return infile_fp.read()
     except OSError:
-        sys.exit(f"[ERROR] File \"{args.infile}\" could not be opened/read")
+        sys.exit(f"[ERROR] File \"{infile}\" could not be opened/read")
 
-def validate_and_fetch_outfile() -> BufferedWriter:
-    if args.outfile is None: # No output filename given
-        args.outfile = pathlib.Path(args.infile.stem).with_suffix('.chf')
-    if args.outfile.exists(): # Outfile already exists
+def validate_and_fetch_outfile(infile: pathlib.Path, outfile: pathlib.Path, yes_flag: bool, no_flag: bool) -> BufferedWriter:
+    if outfile is None: # No output filename given
+        outfile = pathlib.Path(infile.stem).with_suffix('.chf')
+    if outfile.exists():
         response = 'y'
-        if not args.yes and not args.no: # Prompt user
-            response = input(f"File {args.outfile} already exists. Overwrite? [y/N] ")
-        if args.no or response != 'y':
-            sys.exit(f"[ERROR] File \"{args.outfile}\" already exists")
+        if not yes_flag and not no_flag:
+            response = input(f"File {outfile} already exists. Overwrite? [y/N] ")
+        if no_flag or response != 'y':
+            sys.exit(f"[ERROR] File \"{outfile}\" already exists")
         else:
-            print(f"[WARNING] Overwriting \"{args.outfile}\"")    
+            print(f"[WARNING] Overwriting \"{outfile}\"")    
     try:
-        return open(args.outfile, "wb")
+        return open(outfile, "wb")
     except OSError:
-        sys.exit(f"[ERROR] File \"{args.outfile}\" could not be opened/written")
+        sys.exit(f"[ERROR] File \"{outfile}\" could not be opened/written")
 
 def generate_arg_text(packet: Packet) -> str:
-    command = f"--{chip_type_list[packet.chip_type].string}"
+    command = f"--{chip_type_list[packet.chip_type].name}"
     start = hex(packet.load_address)
     size = hex(packet.image_size)
     return f"{command} {start} {size}"
 
-def get_memory_map() -> list[Packet]:
+def get_memory_map(args: argparse.Namespace) -> list[Packet]:
     packets = hardware_type_list[args.hardwaretype].packets
     
     # Memory map provided by user?
-    if True in (bool(getattr(args, chip_type.string)) for chip_type in chip_type_list):
+    if True in (bool(getattr(args, chip_type.name)) for chip_type in chip_type_list):
         if hardware_type_list[args.hardwaretype].manual_memory_map:
             # Build packets
             packets = []
             for chip_type in chip_type_list:
-                for start, size in getattr(args, chip_type.string):
+                for start, size in getattr(args, chip_type.name):
                     packets.append(Packet(chip_type.designation_id, start, size))
 
             # Validate packet ranges
@@ -291,34 +287,26 @@ def create_chf_file(fp: BufferedWriter, chf_data: ChfData, outfile_name: str) ->
 if __name__ == "__main__":
 
     args = parse_args()
-    print(f"\nDEBUG 1: {args}\n")
+    infile_data = validate_and_fetch_infile(args.infile)
+    outfile_fp = validate_and_fetch_outfile(args.infile, args.outfile, args.yes, args.no)
 
-    # Handle infile
-    infile_data = validate_and_fetch_infile()
-
-    # Handle outfile
-    outfile_fp = validate_and_fetch_outfile()
-
-    # Handle title
     if args.title is None:
         args.title = args.infile.stem
     if len(args.title) == 0:
         args.title = 'out'
 
-    # Handle memory map
-    packets = get_memory_map()
-
+    packets = get_memory_map(args)
     packets = map_bin_to_packets(infile_data, packets)
-
     chf_data = ChfData(args.hardwaretype, FORMAT_VERSION, args.title, None, packets)
-    
-    print("\nPackets:")
-    for packet in packets:
-        print("   Type: ", packet.chip_type)
-        print("   Start:", hex(packet.load_address))
-        print("   Size: ", hex(packet.image_size), "\n")
 
-    print(f"\nDEBUG 2: {args}\n")
-    
+    print(f"\nGenerating \"{args.outfile}\":")
+    print(f"  Title: {args.title}")
+    print(f"  Hardware Type: {hardware_type_list[args.hardwaretype].name} [{args.hardwaretype}]")
+    print("  Packets:")
+    for packet in packets:
+        print(f"    Type: {chip_type_list[packet.chip_type].name.upper()} [{packet.chip_type}]", end=', ')
+        print(f"Start: 0x{packet.load_address:x}", end=', ')
+        print(f"Size: 0x{packet.image_size:x} bytes")
+
     create_chf_file(outfile_fp, chf_data, args.outfile)
-    print("Success")
+    print("\nOK")
